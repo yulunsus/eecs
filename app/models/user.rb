661aade6003f0192b2a3bb51bedb677beccb2756
claude51315 <claude51315@gmail.com>
2015-05-05 22:55:59 +0800

    login with username or email (case sensitive)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	validates_uniqueness_of :username 
+	validates_presence_of :username
+	validates :username , length: {in: 4..20}  
+
+	
+	 
+	
+	
+	attr_accessor :login 
+	def self.find_first_by_auth_conditions(warden_conditions)
+		conditions = warden_conditions.dup 
+		if login = conditions.delete(:login)
+			where(conditions.to_hash).where(["username = :value OR email = :value ", { :value => login }]).first
+		else
+			where(conditions.to_hash).first
+		end
+		
+	end
+
 end
