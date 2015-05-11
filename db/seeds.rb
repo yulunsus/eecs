ea10d85fed6e4a278f3c4c9f18cff1c21cfa06dd
ma405th <ma405th@gmail.com>
2015-05-11 10:16:17 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Adminparam.create!(:point_get_feedback=>1,:point_get_pastexam=>1,:point_need_feedback=>0,:point_need_pastexam=>0)
+User.create! :username => 'admin', :email => 'admin@gmail.com', :password => '12345678', :password_confirmation => '12345678' , :is_admin => 1, :nick_name => 'admin', :first_name => 'admin' , :last_name => 'admin'
+
+
