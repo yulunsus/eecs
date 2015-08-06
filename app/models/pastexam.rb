2cf6a070caf137cecff911a65002f54386581000
ma405th <ma405th@gmail.com>
2015-08-06 16:42:23 -0700

    fix pastexam do not upload file bug

--- app/models/pastexam.rb
+++ app/models/pastexam.rb
@@ -4,10 +4,13 @@ class Pastexam < ActiveRecord::Base
   mount_uploader :file, AttachmentUploader
   validate :file_size
   validates_length_of :description, :maximum => 15
+  validates_presence_of :file
 
   def file_size
-    if file.file.size.to_f/(1000*1000) > 50.0
-      errors.add(:file, "You cannot upload a file greater than 50 MB")
+    if file.file
+      if file.file.size.to_f/(1000*1000) > 50.0
+        errors.add(:file, "You cannot upload a file greater than 50 MB")
+      end
     end
   end
   #validates_presence_of :year,:which_time
