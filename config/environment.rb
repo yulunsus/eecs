6399430485de5e2cef8e8f67636d04165a52bb74
ma405th <ma405th@gmail.com>
2015-08-09 01:09:07 -0700

    config error

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.i18n.default_locale = "zh-TW"
+#config.i18n.default_locale = "zh-TW"
